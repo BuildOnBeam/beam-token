069ba86183926ba8e9d51deeb79e60ffa3e8860a
federava <federrava@gmail.com>
2023-10-17 14:56:00 +0200

    chore: add natspec to interface methods

--- contracts/interfaces/IBeamToken.sol
+++ contracts/interfaces/IBeamToken.sol
@@ -5,6 +5,13 @@ import "@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IBeamToken is IERC20, IERC20Permit {
+    /// @notice Mint function
+    /// @param _to Destination of the tokens
+    /// @param _amount Quantity of tokens minted
     function mint(address _to, uint256 _amount) external;
+
+    /// @notice Burn function
+    /// @param _from Source of the tokens
+    /// @param _amount Quantity of tokens burned
     function burn(address _from, uint256 _amount) external;
 }
